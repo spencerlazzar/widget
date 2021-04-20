2652721a64c4dd7d7cb1f6f334f3779b85d1114b
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-04-20 14:58:34 -0700

    List rendering

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,42 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Search = () => {
-  const [term, setTerm] = useState('')
+  const [term, setTerm] = useState('programming')
+  const [results, setResults] = useState([])
 
+  console.log(results)
+
+  useEffect(() => {
+    const search = async () => {
+      const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        }
+      })
+
+      setResults(data.query.search)
+    }
+
+    search()
+  }, [term])
+
+  const renderResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          {result.snippet}
+        </div>
+      </div>
+    )
+  })
 
   return (
     <div>
@@ -15,6 +49,9 @@ const Search = () => {
         className="input"
         />
       </div>
+      </div>
+      <div className="ui celled results">
+        {renderResults}
       </div>
     </div>
   )
