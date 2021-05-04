f53d82dc3e9bd411a6eef72d2a825149f6ee1dea
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-05-03 21:54:01 -0700

    Search on Initial Render

--- src/components/Search.js
+++ src/components/Search.js
@@ -21,11 +21,20 @@ const Search = () => {
 
       setResults(data.query.search)
     }
-    const timeoutId = setTimeout(() => {
+
+    if(term && !results.length) {
+      search()
+    } else {
+          const timeoutId = setTimeout(() => {
       if(term){
         search()
       }
     }, 500)
+    
+    return () => {
+      clearTimeout(timeoutId)
+    }
+    }
   }, [term])
 
   const renderResults = results.map((result) => {
