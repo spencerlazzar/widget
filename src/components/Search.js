c6452fcbd4565e5de1437c8d62fa278a0995c3c2
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-05-03 21:55:51 -0700

    Searching on Inital Render

--- src/components/Search.js
+++ src/components/Search.js
@@ -64,7 +64,7 @@ const Search = () => {
     <div>
       <div className="ui form">
       <div className="field">
-        <label>Enter Search Term:</label>
+        <label>Enter Search Term</label>
         <input 
         value={term}
         onChange={e => setTerm(e.target.value)}
