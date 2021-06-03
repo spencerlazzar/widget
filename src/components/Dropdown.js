f6ae1c0ef66a503de4e4898f9c06d18918e831a6
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-06-03 13:18:13 -0700

    useRef

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,19 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+      document.body.addEventListener("click", (event) => {
+        if(ref.current.contains(event.target)){
+          return;
+        }
+          setOpen(false);
+        },
+        { capture: true }
+      );
+  }, [])
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
@@ -22,7 +34,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
 
 
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
         <div 
