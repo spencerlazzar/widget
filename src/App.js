b846462c66f8c872d7514409d12f89d438547ebd
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-06-04 10:34:51 -0700

    Implemented Header for Navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Accordion from './components/Accordion';
 import Dropdown from './components/Dropdown';
 import Search from './components/Search';
 import Translate from './components/Translate';
+import Route from './components/Route'
+import Header from './components/Header';
 
 
 const items = [
@@ -36,40 +38,31 @@ const options = [
   },
 ]
 
-const showAccordion = () => {
-  if (window.location.pathname === '/') {
-    return <Accordion items={items} />
-  }
-}
-
-const showList = () => {
-  if (window.location.pathname === '/list') {
-    return <Search />
-  }
-}
-
-const showDropdown = () => {
-  if (window.location.pathname === '/dropdown') {
-    return <Dropdown />
-  }
-}
-
-const showTranslate = () => {
-  if (window.location.pathname === '/translate') {
-    return <Translate />
-  }
-}
 
 
 function App() {
+  const [ selected, setSelected ] = useState(0)
+
   return (
     <div className="App">
-      {showAccordion()}
-      {showList()}
-      {showDropdown()}
-      {showTranslate()}
-
-
+      <Header />
+      <Route path="/">
+          <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+          <Search />
+      </Route>
+      <Route path="/dropdown">
+          <Dropdown 
+            label="Select a color" 
+            options={options}
+            selected={selected} 
+            onSelectedChange={setSelected}
+          />
+      </Route>
+      <Route path="/translate">
+          <Translate />
+      </Route>
     </div>
   );
 }
